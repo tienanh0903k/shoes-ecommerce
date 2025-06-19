9a5e26718317e0a1fcf870ff8b686c2de929969f
tienanh0903k <tienanh2003k@gmail.com>
2025-06-19 09:40:58 +0700

    sua dockerfile 5

--- Client/src/environment/environment.docker.ts
+++ Client/src/environment/environment.docker.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://160.191.244.41:5179/api',
-    imgUrl: 'http://160.191.244.41:5179',
-  };
-  
+  production: true,
+  apiUrl: 'http://shoesstore-api:5179/api',
+  imgUrl: 'http://shoesstore-api:5179'
+};
