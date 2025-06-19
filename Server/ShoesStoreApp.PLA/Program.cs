4848f6d0615e179258909fd3c504de9179f46702
tienanh0903k <tienanh2003k@gmail.com>
2025-06-19 08:43:37 +0700

    sua dockerfile 5

--- Server/ShoesStoreApp.PLA/Program.cs
+++ Server/ShoesStoreApp.PLA/Program.cs
@@ -150,11 +150,10 @@ var app = builder.Build();
 // // ======= HẾT PHẦN SEED ROLE =======
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 // app.UseHttpsRedirection();
 
